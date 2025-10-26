744bca7efed3303462917108771dee89f3faf603
mustfamoolan <almoq3m@gmail.com>
2025-10-27 02:25:54 +0300

    feat: Update UserSeeder to create only admin user with production credentials

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,46 +17,11 @@ class UserSeeder extends Seeder
     {
         // Create Admin User
         User::create([
-            'name' => 'أحمد المدير',
+            'name' => 'محمد المدير',
             'email' => 'admin@paranakids.com',
-            'phone' => '0501234567',
-            'password' => Hash::make('password'),
+            'phone' => '07736182383',
+            'password' => Hash::make('mz07736182383'),
             'role' => 'admin',
-        ]);
-
-        // Create Supplier User
-        User::create([
-            'name' => 'محمد المجهز',
-            'email' => 'supplier@paranakids.com',
-            'phone' => '0507654321',
-            'code' => 'SUP001',
-            'password' => Hash::make('password'),
-            'role' => 'supplier',
-        ]);
-
-        // Create Delegate Users
-        User::create([
-            'name' => 'سارة المندوبة',
-            'phone' => '0509876543',
-            'code' => 'DEL001',
-            'password' => Hash::make('password'),
-            'role' => 'delegate',
-        ]);
-
-        User::create([
-            'name' => 'علي المندوب',
-            'phone' => '0504567890',
-            'code' => 'DEL002',
-            'password' => Hash::make('password'),
-            'role' => 'delegate',
-        ]);
-
-        User::create([
-            'name' => 'فاطمة المندوبة',
-            'phone' => '0503210987',
-            'code' => 'DEL003',
-            'password' => Hash::make('password'),
-            'role' => 'delegate',
         ]);
     }
 }
