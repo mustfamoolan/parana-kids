bb3dc7d874706d9764eae087c6e60fde2e9f838a
mustfamoolan <almoq3m@gmail.com>
2025-12-13 00:45:33 +0300

    Add missing Cache import in AlWaseetController

--- app/Http/Controllers/Admin/AlWaseetController.php
+++ app/Http/Controllers/Admin/AlWaseetController.php
@@ -11,6 +11,7 @@ use App\Services\AlWaseetService;
 use App\Services\ProfitCalculator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
