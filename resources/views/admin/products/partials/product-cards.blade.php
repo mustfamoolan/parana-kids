8f654138520b90d6f9e003103eeb7e68e22a56b3
mustfamoolan <samiraalyaseen20@gmail.com>
2026-08-04 13:53:01 +0300

    Fix duplicate migrations causing server crash

--- resources/views/admin/products/partials/product-cards.blade.php
+++ resources/views/admin/products/partials/product-cards.blade.php
@@ -188,9 +188,8 @@
                         @endif
                     </div>
 
-                    <!-- أزرار الإجراء حسب وضع التصفح أو إنشاء الطلب -->
+                    <!-- أزرار الإجراء: يظهر فقط في وضع إنشاء الطلب -->
                     @if(isset($isOrderMode) && $isOrderMode)
-                        <!-- زر إضافة للطلب عند إنشاء طلب -->
                         <button type="button"
                                 onclick="openProductModal({{ $product->id }})"
                                 class="btn btn-primary w-full mt-4">
@@ -199,16 +198,6 @@
                             </svg>
                             إضافة للطلب
                         </button>
-                    @else
-                        <!-- عند تصفح المنتجات فقط: زر عرض التفاصيل -->
-                        <a href="{{ route('admin.warehouses.products.show', [$product->warehouse_id, $product->id]) }}"
-                           class="btn btn-outline-primary w-full mt-4">
-                            <svg class="w-4 h-4 ltr:mr-2 rtl:ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                            </svg>
-                            عرض التفاصيل
-                        </a>
                     @endif
                 </div>
             </div>
